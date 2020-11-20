cbbf12bfc575067515317b73ae1b3bcb924410f7
Philipp van Kronenberg <46745595+dieKrone@users.noreply.github.com>
2020-11-20 18:17:48 +0100

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Nov  9 11:02:06 2020
 
@@ -19,6 +18,7 @@ filename_psych = r'\Psychiatry_Abstraction_List_Consensus.csv'
 
 df_bio = pd.read_csv(root + filename_bio, sep = ';', encoding=r'ISO-8859-1')
 df_psych = pd.read_csv(root + filename_psych, sep = ';', encoding=r'ISO-8859-1')
+df_neuro = pd.read_pickle(r'S:\Fakultaet\MFZ\NWFZ\AGdeHoz\Philipp\Data\Metaresearch\df_neuro.pkl', compression='infer')
 
 col_reason = np.arange(2,201,3)
 
@@ -138,36 +138,186 @@ df.head
 
 fields = ['Neuroscience', 'Biology', 'Psychiatry']
 
-method_cols_bio = np.arange(18, np.size(df_psych, 1), 3)
-method_cols = np.arange(19, np.size(df_psych, 1), 3)
-
-cit_no = []
+method_cols_bio = np.arange(18, np.size(df_bio, 1), 3)
+method_cols_psych = np.arange(19, np.size(df_psych, 1), 3)
+method_cols_neuro = np.arange(17, np.size(df_neuro, 1), 3)
+p_cols_bio = np.arange(19, np.size(df_bio, 1), 3)
+p_cols_psych = np.arange(20, np.size(df_psych, 1), 3)
+p_cols_neuro = np.arange(21, np.size(df_neuro, 1), 3)
+pro_neuro = []
+pos_neuro = []
+no_neuro = []
+pro_bio = []
+pos_bio = []
+no_bio = []
+pro_psych = []
+pos_psych = []
+no_psych = []
+cit_no_psych = []
+cit_no_bio = []
+cit_no_neuro = []
 
 
 
 for i in range(np.size(df_psych, 0)):
     
-    row_temp = np.array(df_psych.iloc[i,method_cols])
+    row_temp = np.array(df_psych.iloc[i,method_cols_psych])
+    p_temp = np.array(df_psych.iloc[i,p_cols_psych])
     
     row_temp = row_temp.astype('float')
     row_temp = row_temp[~np.isnan(row_temp)]
+    p_temp = p_temp.astype('str')
+    pro_idx = np.where(p_temp=='probable')[0]
+    pos_idx = np.where(p_temp=='possible')[0]
+    no_idx = np.where(p_temp=='none')[0]
     
-    cit_no = np.append(cit_no, len(row_temp))
+    cit_no_psych = np.append(cit_no_psych, len(row_temp))
+    pro_psych = np.append(pro_psych, len(pro_idx))
+    pos_psych = np.append(pos_psych, len(pos_idx))
+    no_psych = np.append(no_psych, len(no_idx))
 
+for i in range(np.size(df_neuro, 0)):
+    
+    row_temp = np.array(df_neuro.iloc[i,method_cols_neuro])
+    p_temp = np.array(df_neuro.iloc[i,p_cols_neuro])
+    
+    row_temp = row_temp.astype('float')
+    row_temp = row_temp[~np.isnan(row_temp)]
+    pro_idx = np.where(p_temp=='probable')[0]
+    pos_idx = np.where(p_temp=='possible')[0]
+    no_idx = np.where(p_temp=='none')[0]
+    
+    cit_no_neuro = np.append(cit_no_neuro, len(row_temp))
+    pro_neuro = np.append(pro_neuro, len(pro_idx))
+    pos_neuro = np.append(pos_neuro, len(pos_idx))
+    no_neuro = np.append(no_neuro, len(no_idx))
 
+for i in range(np.size(df_bio, 0)):
+    
+    row_temp = np.array(df_bio.iloc[i,method_cols_bio])
+    p_temp = np.array(df_bio.iloc[i,p_cols_bio])
+    
+    row_temp = row_temp.astype('float')
+    row_temp = row_temp[~np.isnan(row_temp)]
+    pro_idx = np.where(p_temp=='probable')[0]
+    pos_idx = np.where(p_temp=='possible')[0]
+    no_idx = np.where(p_temp=='none')[0]
+    
+    cit_no_bio = np.append(cit_no_bio, len(row_temp))
+    pro_bio = np.append(pro_bio, len(pro_idx))
+    pos_bio = np.append(pos_bio, len(pos_idx))
+    no_bio = np.append(no_bio, len(no_idx))
+    
+    
+array_psych = np.zeros(len(cit_no_bio)-len(cit_no_psych))
+array_psych[:] = np.nan
+cit_no_psych = np.concatenate((cit_no_psych, array_psych))
+pro_psych = np.concatenate((pro_psych, array_psych))
+pos_psych = np.concatenate((pos_psych, array_psych))
+no_psych = np.concatenate((no_psych, array_psych))
 
-dict = {'cit_no': cit_no}
+array_neuro = np.zeros(len(cit_no_bio)-len(cit_no_neuro))
+array_neuro[:] = np.nan
+cit_no_neuro = np.concatenate((cit_no_neuro, array_neuro))
+pro_neuro = np.concatenate((pro_neuro, array_neuro))
+pos_neuro = np.concatenate((pos_neuro, array_neuro))
+no_neuro = np.concatenate((no_neuro, array_neuro))
+
+dict = {'neuro': cit_no_neuro,
+        'bio': cit_no_bio,
+        'psych': cit_no_psych}
+
+dict_pro = {'pro_neuro': pro_neuro,
+            'pro_bio': pro_bio,
+            'pro_psych': pro_psych}
+
+dict_pos = {'pos_neuro': pos_neuro,
+            'pos_bio': pos_bio,
+            'pos_psych': pos_psych}
+
+dict_no = {'no_neuro': no_neuro,
+           'no_bio': no_bio,
+           'no_psych': no_psych}
 
 data = pd.DataFrame.from_dict(dict)
 
+d_pro = pd.DataFrame.from_dict(dict_pro)
+d_pos = pd.DataFrame.from_dict(dict_pos)
+d_no = pd.DataFrame.from_dict(dict_no)
+
+Neuro_7colors = np.array(['#FFFCEB',
+                            '#FFF6C2',
+                            '#FFED85',
+                            '#FFE347',
+                            '#E0BF00',
+                            '#B89C00',
+                            '#8F7900'])
+
+ 
+
+Psych_7colors = np.array(['#FFEEEB',
+                            '#FFCBC2',
+                            '#FF9785',
+                            '#FF6347',
+                            '#E02200',
+                            '#B81C00',
+                            '#8F1500'])
+
+ 
+
+Bio_7colors = np.array(['#EDF3FD',
+                            '#C8DAF9',
+                            '#90B5F3',
+                            '#598FEE',
+                            '#1558CB',
+                            '#1248A5',
+                            '#0E3881'])
+
+Neuro_4colors =  Neuro_7colors[[0, 2, 4, 6]]
+Psych_4colors =  Psych_7colors[[0, 2, 4, 6]]
+Bio_4colors =  Bio_7colors[[0, 2, 4, 6]]
+
+Neuro_3colors =  Neuro_7colors[[0, 3, 6]]
+Psych_3colors =  Psych_7colors[[0, 3, 6]]
+Bio_3colors =  Bio_7colors[[0, 3, 6]]
+
+Neuro_1color =  Neuro_7colors[3]
+Psych_1color =  Psych_7colors[3]
+Bio_1color =  Bio_7colors[3]
+
+
 sns.set_style("ticks")
-my_pal2 = {"cit_no": 'green'}
+my_pal2 = {"neuro": Neuro_1color, 'bio': Bio_1color, 'psych': Psych_1color}
 plt.figure()
-ax1 = sns.violinplot(data=data, palette=my_pal2)
-plt.title('Biology - number of citations per publication')
+ax = sns.violinplot(data=data, palette=my_pal2)
+
+for violin, alpha in zip(ax.collections[::2], [0.5, 0.5, 0.5]):
+    violin.set_alpha(alpha)
+    
+plt.title('Number of citations per publication over fields')
 plt.ylabel('number of citations')
 
 
+#probable, possible, none
+s_types = ['pro', 'pos', 'no']
+s_type_full = ['probable', 'possible', 'none']
+data = [d_pro, d_pos, d_no]
 
+for i, typ in enumerate(s_types):
+    sns.set_style("ticks")
+    my_pal = {"{}_neuro".format(typ): Neuro_1color, '{}_bio'.format(typ): Bio_1color, '{}_psych'.format(typ): Psych_1color}
+    plt.figure()
+    ax = sns.violinplot(data=data[i], palette=my_pal)
+    
+    for violin, alpha in zip(ax.collections[::2], [0.5, 0.5, 0.5]):
+        violin.set_alpha(alpha)
+        
+    plt.title('Number of {}-shortcuts per publication over fields'.format(s_type_full[i]))
+    plt.ylabel('number of citations')
+    plt.ylim((-2.5,18))
+
+np.nansum(cit_no_neuro)
+np.nansum(cit_no_bio)
+np.nansum(cit_no_psych)
 
 
